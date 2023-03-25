6dac0cb5665659475701c05dd1d0bd4bc25dc073
tanjim00 <tanjim.ac00@gmail.com>
2023-03-25 16:25:06 +0600

    image cards implemented through api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-
+import React, { useEffect, useState } from "react";
+import ImageCard from "./components/ImageCard";
 
 function App() {
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    .then(res => res.json())
+    .then(data => {
+      setImages(data.hits);
+      setIsLoading(false);
+    })
+    .catch(err => console.log(err));
+  },[]);
+
   return (
-    <div className="App">
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image}/>
+        ))}
+      </div>
     </div>
   );
 }
